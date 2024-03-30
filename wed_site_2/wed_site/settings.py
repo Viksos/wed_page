ab874806e60d476f1e4070e6ddc429207dd0d02c
Viksos <45750858+Viksos@users.noreply.github.com>
2024-03-30 22:42:01 +0100

    Update settings.py

--- wed_site_2/wed_site/settings.py
+++ wed_site_2/wed_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-55h$zd%7c1z&&75hsv4*4xkot%cib!y9ep!s6$%f##%0iw=2u&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
